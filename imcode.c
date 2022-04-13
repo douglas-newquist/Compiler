13991d991cb271458e83eb2d65e812ddc6b8c44e
Douglas Newquist <deathsnake@protonmail.com>
2022-04-12 19:57:03 -0600

    For loops

--- imcode.c
+++ imcode.c
@@ -17,7 +17,7 @@ int i = 0;
 int has_main = FALSE;
 int offset = 0;
 
-void populate_code(ICode *code, SymbolTable *scope, Tree *tree);
+Address *populate_code(ICode *code, SymbolTable *scope, Tree *tree);
 
 void generate_children_code(ICode *code, SymbolTable *scope, Tree *tree)
 {
@@ -28,49 +28,36 @@ void generate_children_code(ICode *code, SymbolTable *scope, Tree *tree)
 		populate_code(code, scope, tree->children[i]);
 }
 
-Address *parse_address(ICode *code, SymbolTable *scope, Tree *tree)
+void builtin_call(ICode *code, Symbol *function, Tree *tree)
 {
+}
+
+Address *populate_code(ICode *code, SymbolTable *scope, Tree *tree)
+{
+	if (tree == NULL)
+		return NULL;
+
 	Symbol *symbol = NULL;
+	Instruction *i1, *i2;
 	Address *a1 = NULL, *a2 = NULL, *a3 = NULL;
+
+	if (tree->token)
+		set_pos(tree->token);
 
 	switch (tree->rule)
 	{
 	case ID:
 		symbol = lookup(scope, tree->token->text, SCOPE_SYMBOLS);
+
+		if (tree->token->symbol)
+		{
+			symbol = tree->token->symbol;
+			if (symbol->attributes & ATR_VARIABLE)
+				LIST_ADD(code->instructions,
+						 create_instruction(I_ASSIGN, symbol->address, Const(0), NULL));
+		}
+
 		return symbol->address;
-
-	case R_OP1_DECREMENT:
-		a2 = parse_address(code, scope, tree->children[0]);
-		a1 = create_address(RE_LOCAL, (offset++) * BYTE_SIZE);
-		LIST_ADD(code->instructions, create_instruction(R_OP2_SUB, a1, a2, create_address(RE_CONST, 1)));
-		return a1;
-
-	case R_OP1_INCREMENT:
-		a2 = parse_address(code, scope, tree->children[0]);
-		a1 = create_address(RE_LOCAL, (offset++) * BYTE_SIZE);
-		LIST_ADD(code->instructions, create_instruction(R_OP2_ADD, a1, a2, create_address(RE_CONST, 1)));
-		return a1;
-
-	case R_OP2_ADD:
-	case R_OP2_AND:
-	case R_OP2_DIV:
-	case R_OP2_EQUALS:
-	case R_OP2_GREATER_EQUAL:
-	case R_OP2_GREATER:
-	case R_OP2_LESS_EQUAL:
-	case R_OP2_LESS:
-	case R_OP2_MOD:
-	case R_OP2_MULT:
-	case R_OP2_NOT_EQUAL:
-	case R_OP2_OR:
-	case R_OP2_SUB:
-		a3 = parse_address(code, scope, tree->children[1]);
-	case R_OP1_NEGATE:
-	case R_OP1_NOT:
-		a2 = parse_address(code, scope, tree->children[0]);
-		a1 = create_address(RE_LOCAL, (offset++) * BYTE_SIZE);
-		LIST_ADD(code->instructions, create_instruction(tree->rule, a1, a2, a3));
-		return a1;
 
 	case LITERAL_BOOL:
 		return create_address(RE_CONST, tree->token->bval);
@@ -87,55 +74,82 @@ Address *parse_address(ICode *code, SymbolTable *scope, Tree *tree)
 
 	case LITERAL_NULL:
 		return create_address(RE_CONST, 0);
-	}
-	return NULL;
-}
 
-void builtin_call(ICode *code, Symbol *function, Tree *tree)
-{
-}
+	case R_OP1_DECREMENT:
+		a1 = populate_code(code, scope, tree->children[0]);
+		LIST_ADD(code->instructions, create_instruction(R_OP2_SUB, a1, a1, create_address(RE_CONST, 1)));
+		return a1;
 
-void populate_code(ICode *code, SymbolTable *scope, Tree *tree)
-{
-	Symbol *symbol = NULL;
-	Instruction *i1, *i2;
-	Address *a1 = NULL, *a2 = NULL, *a3 = NULL;
+	case R_OP1_INCREMENT:
+		a1 = populate_code(code, scope, tree->children[0]);
+		LIST_ADD(code->instructions, create_instruction(R_OP2_ADD, a1, a1, create_address(RE_CONST, 1)));
+		return a1;
 
-	switch (tree->rule)
-	{
+	case R_OP2_ADD:
+	case R_OP2_AND:
+	case R_OP2_DIV:
+	case R_OP2_EQUALS:
+	case R_OP2_GREATER_EQUAL:
+	case R_OP2_GREATER:
+	case R_OP2_LESS_EQUAL:
+	case R_OP2_LESS:
+	case R_OP2_MOD:
+	case R_OP2_MULT:
+	case R_OP2_NOT_EQUAL:
+	case R_OP2_OR:
+	case R_OP2_SUB:
+		a3 = populate_code(code, scope, tree->children[1]);
+	case R_OP1_NEGATE:
+	case R_OP1_NOT:
+		a2 = populate_code(code, scope, tree->children[0]);
+		a1 = create_address(RE_LOCAL, (offset++) * BYTE_SIZE);
+		LIST_ADD(code->instructions, create_instruction(tree->rule, a1, a2, a3));
+		return a1;
+
 	case PUBLIC:
 	case R_EMPTY:
 	case STATIC:
 		break;
 
 	case R_ASSIGN1:
-		a3 = parse_address(code, scope, tree->children[1]);
+		a1 = populate_code(code, scope, tree->children[0]);
+		a2 = populate_code(code, scope, tree->children[1]);
 		LIST_ADD(code->instructions, create_instruction(I_ASSIGN, a1, a2, a3));
 		break;
 
 	case R_CALL1:
 		symbol = lookup_name(scope, tree->children[0], SCOPE_SYMBOLS);
+		switch (tree->children[1]->rule)
+		{
+		case R_EMPTY:
+			break;
+
+		case R_ARG_GROUP:
+			break;
+
+		default:
+			LIST_ADD(code->instructions,
+					 create_instruction(I_PARAM,
+										populate_code(code, scope, tree->children[1]),
+										NULL,
+										NULL));
+			break;
+		}
+
 		if (symbol->attributes & ATR_BUILTIN)
 		{
 			builtin_call(code, symbol, tree);
 			a1 = create_label_address(symbol->text);
 		}
 		else
+		{
 			a1 = create_label_address(symbol->start_label);
+		}
+
 		a2 = create_address(RE_CONST, symbol->type->info.method.count);
 		a3 = create_address(RE_LOCAL, (offset++) * BYTE_SIZE);
 		LIST_ADD(code->instructions, create_instruction(I_CALL, a1, a2, a3));
-		break;
-
-	case ID:
-		if (tree->token->symbol)
-		{
-			symbol = tree->token->symbol;
-			if (symbol->attributes & ATR_VARIABLE)
-				LIST_ADD(code->instructions,
-						 create_instruction(I_ASSIGN, symbol->address, Const(0), NULL));
-		}
-		break;
+		return a3;
 
 	case R_DEFINE2:
 		generate_children_code(code, scope, tree);
@@ -146,7 +160,7 @@ void populate_code(ICode *code, SymbolTable *scope, Tree *tree)
 		LIST_ADD(code->instructions,
 				 create_instruction(I_ASSIGN,
 									symbol->address,
-									parse_address(code, scope, tree->children[0]),
+									populate_code(code, scope, tree->children[0]),
 									NULL));
 		break;
 
@@ -161,33 +175,26 @@ void populate_code(ICode *code, SymbolTable *scope, Tree *tree)
 	case R_CLASS1:
 		symbol = lookup(scope, tree->token->text, SCOPE_SYMBOLS);
 		scope = symbol->type->info.class.scope;
-		symbol->start_label = message("%s%d:", tree->token->text, i++);
 		LIST_ADD(code->instructions, create_label(I_LABEL, symbol->start_label));
 		generate_children_code(code, scope, tree);
 		break;
 
 	case R_METHOD1:
-		offset = 0;
 		symbol = lookup(scope, tree->token->text, SCOPE_SYMBOLS);
 		scope = symbol->type->info.method.scope;
+		offset = scope->symbols->count;
 		if (strcmp(tree->token->text, "main") == 0)
 		{
 			has_main = TRUE;
 			LIST_ADD(code->instructions, create_label(I_LABEL, "start:"));
 		}
-		symbol->start_label = message("%s_start%d:", tree->token->text, i++);
-		symbol->end_label = message("%s_end%d:", tree->token->text, i - 1);
 		LIST_ADD(code->instructions, create_label(I_LABEL, symbol->start_label));
 		populate_code(code, scope, tree->children[4]);
-		LIST_ADD(code->instructions, create_label(I_LABEL, symbol->end_label));
 		LIST_ADD(code->instructions, create_instruction(I_RETURN, NULL, NULL, NULL));
 		break;
 
-	case R_OP1_INCREMENT:
-		break;
-
 	case R_IF1:
-		a1 = parse_address(code, scope, tree->children[0]);
+		a1 = populate_code(code, scope, tree->children[0]);
 		a2 = create_label_address(message("%d:", i++));
 		i1 = create_instruction(I_JUMP_FALSE, a1, a2, NULL);
 		LIST_ADD(code->instructions, i1);
@@ -197,7 +204,23 @@ void populate_code(ICode *code, SymbolTable *scope, Tree *tree)
 
 	case R_FOR:
 		populate_code(code, scope, tree->children[0]);
+		i1 = create_label(I_LABEL, message("loop_body%d:", i++));
+		i2 = create_label(I_LABEL, message("loop_if%d:", i++));
+		LIST_ADD(code->instructions,
+				 create_instruction(I_JUMP,
+									create_label_address(i2->extra.label.name),
+									NULL,
+									NULL));
+		LIST_ADD(code->instructions, i1);
 		populate_code(code, scope, tree->children[3]);
+		populate_code(code, scope, tree->children[2]);
+		LIST_ADD(code->instructions, i2);
+		populate_code(code, scope, tree->children[1]);
+		LIST_ADD(code->instructions,
+				 create_instruction(I_JUMP_TRUE,
+									populate_code(code, scope, tree->children[1]),
+									create_label_address(i1->extra.label.name),
+									NULL));
 		break;
 
 	case R_CASE_GROUP:
@@ -207,18 +230,17 @@ void populate_code(ICode *code, SymbolTable *scope, Tree *tree)
 		break;
 
 	case R_RETURN2:
-		a1 = parse_address(code, scope, tree->children[0]);
-		LIST_ADD(code->instructions, create_instruction(I_RETURN, a1, NULL, NULL));
-		break;
-
+		a1 = populate_code(code, scope, tree->children[0]);
 	case R_RETURN1:
-		LIST_ADD(code->instructions, create_instruction(I_RETURN, NULL, NULL, NULL));
+		LIST_ADD(code->instructions, create_instruction(I_RETURN, a1, NULL, NULL));
 		break;
 
 	default:
 		error(SEMATIC_ERROR, message("Undefined code generation rule %d", tree->rule));
 		break;
 	}
+
+	return NULL;
 }
 
 ICode *generate_code(SymbolTable *scope, Tree *tree)
@@ -236,15 +258,22 @@ ICode *generate_code(SymbolTable *scope, Tree *tree)
 	if (!has_main)
 		error_at(tree->token, SEMATIC_ERROR, "Program does not contain a main method");
 
+#ifdef DEBUG
+	printf("Intermediate code generation done\n");
+#endif
+
 	return code;
 }
 
 void print_code(ICode *code)
 {
 	printf(".string\n");
+
 	foreach_list(instr, code->strings)
-		print_instruction((Instruction *)instr->value);
+		printf("    %s\\0\n", (char *)instr->value);
+
 	printf(".code\n");
+
 	foreach_list(instr, code->instructions)
 		print_instruction((Instruction *)instr->value);
 }
